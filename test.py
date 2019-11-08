10169267cbf1e5125e8da663d1c0927cfe1037f2
Michael Milton <ttmigueltt@gmail.com>
2019-11-08 19:01:26 +1100

    Remove unused file

--- test.py
+++
@@ -1,8 +0,0 @@
-# def test_person(person):
-#     assert type(person).__name__ == 'Person'
-#
-# def test_computer(computer):
-#     assert type(computer).__name__ == 'Computer'
-#
-# def test_get_list(client, register_routes, person, person_2):
-#     assert True
